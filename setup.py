8cc6bf6efc4884a6ab77077b501923346043905b
xuyong1 <xuyong1@wps.cn>
2020-06-08 16:55:29 +0800

    v1.1.1 修改setup文件

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-version = '1.1.0'
+version = '1.1.1'
 
 if not version:
     raise RuntimeError('Cannot find version information')
@@ -9,6 +9,9 @@ install_requires = []
 with open('requirements.txt', 'r') as f:
     for req in f.readlines():
         install_requires.append(req.strip())
+
+with open("README.md", "r", encoding='utf-8') as fh:
+    long_description = fh.read()
 
 setup(
     name='opstools2',
@@ -19,6 +22,8 @@ setup(
     url='https://xuyonggit.github.io/opstools2/',
     packages=find_packages(exclude=['examples', 'tests']),
     description="运维工具包",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     include_package_data=True,
     classifiers=[
         "Programming Language :: Python :: 3",
