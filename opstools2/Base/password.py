cb4a6b351fc7f5c7a011a72c7100f3ff3fae4740
Administrator <yong.xu@chaitin.com>
2020-11-02 12:14:50 +0800

    版本：1.1.3

--- opstools2/Base/password.py
+++ opstools2/Base/password.py
@@ -7,70 +7,74 @@ class MakePassword(object):
         pass
 
     @staticmethod
-    def __get_num(num):
+    def _get_num(num):
+        _l = []
         templates = "0123456789"
-        return random.sample(templates, num)
+        for i in range(num):
+            _l.append(random.sample(templates, 1)[0])
+        return _l
 
     @staticmethod
-    def __get_lword(num):
-        L = []
+    def _get_lword(num):
+        templates = []
+        _l = []
         for i in range(97, 123):
-            L.append(chr(i))
-        return random.sample(L, num)
+            templates.append(chr(i))
+        for i in range(num):
+            _l.append(random.sample(templates, 1)[0])
+        return _l
 
     @staticmethod
-    def __get_bword(num):
-        L = []
+    def _get_bword(num):
+        templates = []
+        _l = []
         for i in range(65, 91):
-            L.append(chr(i))
-        return random.sample(L, num)
+            templates.append(chr(i))
+        for i in range(num):
+            _l.append(random.sample(templates, 1)[0])
+        return _l
 
     @staticmethod
-    def __get_fuhao(num):
-        S = "!~`@$^&*()_+}{[]:;?/>.<,|"
-        return random.sample(S, num)
+    def _get_fuhao(num):
+        _l = []
+        templates = "!~`@$^&*()_+}{[]:;?/>.<,|"
+        for i in range(num):
+            _l.append(random.sample(templates, 1)[0])
+        return _l
+
+    @staticmethod
+    def _make_pwd(pwd_list):
+        passwd = ""
+        temp_l = [x for x in range(0, len(pwd_list))]
+        while len(temp_l) > 0:
+            l = random.sample(temp_l, 1)
+            passwd = passwd + str(pwd_list[l[0]])
+            temp_l.remove(l[0])
+        return passwd
 
     def getsamplepassword(self, num=8):
-        self.passwd = ""
         # 1/2 数字 & 1/2 小写字母 & other 小写字母
         nums = num // 2
         lw = num // 2
         other = num % 2
-        listofpasswd = self.__get_num(nums) + self.__get_lword(lw) + self.__get_lword(other)
-        temp_l = [x for x in range(0, len(listofpasswd))]
-        while len(temp_l) > 0:
-            l = random.sample(temp_l, 1)
-            self.passwd = self.passwd + str(listofpasswd[l[0]])
-            temp_l.remove(l[0])
-        return self.passwd
+        listofpasswd = self._get_num(nums) + self._get_lword(lw) + self._get_lword(other)
+        return self._make_pwd(listofpasswd)
 
     def getpassword(self, num=12):
-        self.passwd = ""
         # 1/3 数字 & 1/3 小写字母 & 1/3 大写字母  & other 小写字母
         nums = num // 3
         lw = num // 3
         bw = num // 3
         other = num % 3
-        listofpasswd = self.__get_num(nums) + self.__get_lword(lw) + self.__get_bword(bw) + self.__get_lword(other)
-        temp_l = [x for x in range(0, len(listofpasswd))]
-        while len(temp_l) > 0:
-            l = random.sample(temp_l, 1)
-            self.passwd = self.passwd + str(listofpasswd[l[0]])
-            temp_l.remove(l[0])
-        return self.passwd
+        listofpasswd = self._get_num(nums) + self._get_lword(lw) + self._get_bword(bw) + self._get_lword(other)
+        return self._make_pwd(listofpasswd)
 
     def gethardpassword(self, num=18):
-        self.passwd = ""
-        # 1/5 数字 & 1/5 小写字母 & 1/5 大写字母 1/5 特殊符号 & other 小写字母
-        nums = num // 5
-        lw = num // 5
-        bw = num // 5
-        fh = num // 5
-        other = num % 5
-        listofpasswd = self.__get_num(nums) + self.__get_lword(lw) + self.__get_bword(bw) + self.__get_fuhao(fh) + self.__get_lword(other)
-        temp_l = [x for x in range(0, len(listofpasswd))]
-        while len(temp_l) > 0:
-            l = random.sample(temp_l, 1)
-            self.passwd = self.passwd + str(listofpasswd[l[0]])
-            temp_l.remove(l[0])
-        return self.passwd
+        # 1/4 数字 & 1/4 小写字母 & 1/4 大写字母 1/4 特殊符号 & other 小写字母
+        nums = num // 4
+        lw = num // 4
+        bw = num // 4
+        fh = num // 4
+        other = num % 4
+        listofpasswd = self._get_num(nums) + self._get_lword(lw) + self._get_bword(bw) + self._get_fuhao(fh) + self._get_lword(other)
+        return self._make_pwd(listofpasswd)
